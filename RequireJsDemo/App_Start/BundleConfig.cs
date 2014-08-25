034dc351a2c6999c689c405040d8f86580b87987
xuehuibo <xuehuibo@qq.com>
2014-08-25 23:22:40 +0800

    requiredemo

---
+++ RequireJsDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace RequireJsDemo
+{
+    public class BundleConfig
+    {
+        // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/require").Include(
+                        "~/Scripts/require.js"));
+        }
+    }
+}
---
+++ RequireJsDemo/Scripts/main.js
@@ -0,0 +1,35 @@
+﻿console.info("main加载成功");
+require.config({
+    paths: {
+        "jquery": "/Scripts/jquery/jquery-1.8.2.min",
+        "backbone": "/Scripts/Backbone/backbone-min",
+        "underscore": "/Scripts/Underscore/underscore-min",
+        'domready': '/Scripts/Plugin/domReady',//插件 作用：dom加载完毕执行
+        'text':'/Scripts/Plugin/text'//插件 作用：加载文本
+    },
+    shim: {
+        'underscore': {
+            exports:'_'
+        },
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports:'Backbone'
+        }
+    }
+});
+require(['jquery', 'underscore', 'backbone'],
+    function($, _, Backbone) {
+        console.info("require 1");
+    });
+
+require(['text!/Content/template/template1.template'], function (temp) {
+    $("body").html(temp);
+});
+
+require(['domready!'], function(doc) {
+    console.info(doc);
+    require(['jquery', 'underscore', 'backbone'],
+    function ($, _, Backbone) {
+        console.info("require 2");
+    });
+});
